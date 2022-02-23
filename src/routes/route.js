f32ebb629273bb970cce3c08acded59de937e2af
12-shrati <shrati02@gmail.com>
2022-02-23 20:50:59 +0530

    ass3 feb23

--- src/routes/route.js
+++ src/routes/route.js
@@ -1,27 +1,211 @@
-let obj=require('../logger/logger')
-let obj1=require('../util/helper.js')
-let obj2=require('../validator/formatter')
-let obj3=require('../loader')
 const express = require('express');
+const  sum  = require('lodash');
 const router = express.Router();
 
-router.get('/test-me', function (req, res) {
-    obj.printMessage("welcome to my application.I am Shrati and part of Functionup Thorium cohort")
-    obj1.information("thorium W3 D1 The topics for today is node.js module system")
-    obj1.date()
-    obj1.month()
-    obj2.str()
-    obj2.string()
-    obj2.string1()
-    res.send('My first ever api!')
-});
 
-router.get('/hello', function (req, res) {
-    obj3.chunk()
-    obj3.tail()
-    obj3.lod()
-    obj3.pairs()
-        res.send('hello there!')
-    });
+router.get('/sol1', function(req, res) {
+
+    let arr=[1,2,3,5,6,7]
+     let sum=0
     
+    for(let i=0;i<arr.length;i++){
+        sum += arr[i]
+    }
+    let lastDigit=arr.pop()
+    let totalSum=lastDigit*(lastDigit+1)/2
+    let missingNumber=totalSum-sum
+    
+        res.send( {data: missingNumber} )
+})
+    
+
+
+router.get('/sol2', function(req, res) {
+
+    let arr2=[33,34,35,37,38]
+    let len=arr2.length
+    let total=0
+    for(let i=0;i<arr2.length;i++){
+        total +=arr2[i]
+    }
+    let first=arr2[0]
+    let last=arr2.pop()
+    let consecutNumber=(len+1)*(first+last)/2
+    let missingNumber2=consecutNumber-total
+    res.send( {data:missingNumber2} )
+
+})
+
+
+let players=[{
+
+    "name": "manish",
+  
+    "dob": "1/1/1995",
+  
+    "gender": "male",
+  
+    "city": "jalandhar",
+  
+    "sports": [
+  
+      "swimming"
+  
+    ],
+  
+    "bookings": [
+        
+  
+      
+  
+    ]
+  
+  },
+   
+    
+   {
+  
+    "name": "manish",
+  
+    "dob": "1/1/1995",
+  
+    "gender": "male",
+  
+    "city": "jalandhar",
+  
+    "sports": [
+  
+      "swimming"
+  
+    ],
+  
+    "bookings": [
+    {
+      
+   "bookingumber": 1,
+  
+    "sportId": "",
+  
+    "centerId": "",
+  
+   "type": "private",
+  
+   "slot": "16286598000000",
+  
+   "bookedOn": "31/08/2021",
+  
+   "bookedFor": "01/09/2021"
+    },
+    
+      
+  {
+  
+   "bookingNumber": 2,
+  
+    "sportId": "",
+  
+    "centerId": "",
+  
+   "type": "private",
+  
+   "slot": "16286518000000",
+  
+   "bookedOn": "31/08/2001",
+  
+   "bookedFor": "01/09/2001"
+  
+  }
+    
+]
+   },
+  {
+  
+    "name": "gopal",
+  
+    "dob": "1/09/1995",
+  
+    "gender": "male",
+  
+    "city": "delhi",
+  
+    "sports": [
+  
+      "soccer"
+  
+    ],
+  
+    "bookings": [
+       
+  
+    ]
+  
+  },
+  
+  {
+  
+    "name": "lokesh",
+  
+    "dob": "1/1/1990",
+  
+    "gender": "male",
+  
+    "city": "mumbai",
+  
+    "sports": [
+  
+      "soccer"
+  
+    ],
+  
+    "bookings": [
+  
+       
+    ]
+  
+  },
+     
+  ]
+
+
+ router.post('/players', function(req, res){
+     
+     
+     
+     for(let i=0;i<players.length;i++){
+         let name=players[i].name
+         if(name!=players[i].name){
+            let ele=req.body.element
+            players.push(ele)        
+         res.send( { data:players, status:true} )
+        }else{
+            res.send("player already exist")
+        }
+    } 
+ })
+
+
+
+ router.post('/players/:playerName/bookings/:bookingId', function(req, res){
+     let value=req.params.playerName;
+     let id=req.params.bookingId
+     for(i=0;i<players.length;i++){
+         if(players[i].name!=value){
+            res.send("player not found")
+             break
+         } if(players[i].name==value && players[i].bookings.bookingNumber==id){
+             
+             res.send("id already exist")
+         }
+
+         }
+     
+        
+
+
+ })
+  
+
+
+
+
 module.exports = router;

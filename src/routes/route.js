c8cf78dddcaaebbeac6e66a4b76fb2a9e9a3ff80
12-shrati <shrati02@gmail.com>
2022-02-24 20:37:16 +0530

    assignment-feb-24

--- src/routes/route.js
+++ src/routes/route.js
@@ -1,27 +1,65 @@
-let obj=require('../logger/logger')
-let obj1=require('../util/helper.js')
-let obj2=require('../validator/formatter')
-let obj3=require('../loader')
 const express = require('express');
 const router = express.Router();
 
-router.get('/test-me', function (req, res) {
-    obj.printMessage("welcome to my application.I am Shrati and part of Functionup Thorium cohort")
-    obj1.information("thorium W3 D1 The topics for today is node.js module system")
-    obj1.date()
-    obj1.month()
-    obj2.str()
-    obj2.string()
-    obj2.string1()
-    res.send('My first ever api!')
-});
 
-router.get('/hello', function (req, res) {
-    obj3.chunk()
-    obj3.tail()
-    obj3.lod()
-    obj3.pairs()
-        res.send('hello there!')
-    });
+
+
+
+// you will be given an array of persons ( i.e an array of objects )..each person will have  a {name: String , age: Number, votingStatus: true/false(Boolean)}
+// take input in query param as votingAge..and for all the people above that age, change votingStatus as true
+// also return an array consisting of only the person that can vote
+
+// WRITE A POST API TO THE ABOVE
+
+
+// take this as sample for array of persons:
+let persons= [
+   {
+   name: "PK",
+   age: 10,
+   votingStatus: false
+},
+{
+   name: "SK",
+   age: 20,
+   votingStatus: false
+},
+{
+   name: "AA",
+   age: 70,
+   votingStatus: false
+},
+{
+   name: "SC",
+   age: 5,
+   votingStatus: false
+},
+{
+   name: "HO",
+   age: 40,
+   votingStatus: false
+}
+]
+
+router.post('/query-api', function(req, res) {
+let input=req.query.input
+let newArr=[]
+
+
+for(let i=0;i<persons.length;i++){
+    if(persons[i].age>18){
+        persons[i].votingStatus=true
+    }
+    if(persons[i].age>input)
+        newArr.push(persons[i])
+    
+
+}
+ console.log(newArr)
+  res.send(newArr)  
+
+})
+
     
 module.exports = router;
+

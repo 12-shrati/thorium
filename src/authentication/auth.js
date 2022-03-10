e64e0997fba57cc0879aac12e514c3fe4aaa1e52
12-shrati <shrati02@gmail.com>
2022-03-10 19:32:36 +0530

    10 march-assignment

--- src/authentication/auth.js
+++ src/authentication/auth.js
@@ -1,17 +1,23 @@
 const jwt=require('jsonwebtoken')
 
 let authentication=function(req,res,next){
-  let userId=req.params.userId
-   let token = req.headers["x-auth-token"];
-    if (!token) return res.send({ msg: "token must be present" });
+  try{
+      let userId=req.params.userId
+      let token = req.headers["x-auth-token"];
+      if (!token) return res.status(401).send({ msg: "token must be present" });
     
-    let decodedToken = jwt.verify(token, "shrati-marathi");
-    console.log(decodedToken)
-    if (!decodedToken)
-      return res.send({ status: false, msg: "token is invalid" });
+      let decodedToken = jwt.verify(token, "shrati-marathi");
+      console.log(decodedToken)
+     if (!decodedToken)
+      return res.status(401).send({ status: false, msg: "token is invalid" });
 
-      if(decodedToken.userId != userId) return res.send("unathorized access")
+      if(decodedToken.userId != userId) return res.status(403).send("unathorized access")
       req.userId=userId
       next()
+  }
+  catch(error){
+    res.status(500).send({msg:error})
+  }
 }
+
 module.exports.authentication=authentication
--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -2,44 +2,75 @@ const userModel=require("../models/userSchema")
 const jwt=require("jsonwebtoken")
 
 const createUsers=async function(req, res) {
+try{ 
     var data=req.body
+    if(Object.keys(data).length != 0){
     let saveData= await userModel.create(data)
-    res.send({msg:saveData})     
+    res.status(201).send({msg:saveData})
+    }else{
+      res.status(400).send({msg:"data must be present"})
+    }
+}
+catch(error){
+  res.status(500).send({msg:error})
+}     
   }
 
-  const login= async function(req,res) {
-    let emailId=req.body.emailId
-    let password=req.body.password
-    let user = await userModel.findOne({ emaiId:emailId, password:password });
-     if (!user)
-    return res.send({msg: "username or the password is not corerct"});
 
-    let token = jwt.sign({userId: user._id.toString()},
-      "shrati-marathi"
-    );
-    res.setHeader("x-auth-token", token);
-    res.send({ status: true, data: token });
+
+  const login= async function(req,res) {
+    try{
+         let emailId=req.body.emailId
+         let password=req.body.password
+         let user = await userModel.findOne({ emaiId:emailId, password:password });
+         if (!user){
+             return res.status(401).send({msg: "username or the password is not corerct"});
+         }else{
+             let token = jwt.sign({userId: user._id.toString()},"shrati-marathi");
+             res.setHeader("x-auth-token", token);
+             res.status(200).send({ status: true, data: token });
+         }
+    }
+    catch(error){
+      res.status(500).send({msg:error})
+    }
   };
   
   
-  let checkToken= async function (req, res) {    
+  let checkToken= async function (req, res) { 
+    try{   
     let userDetails = await userModel.findById(req.userId);
-    if (!userDetails)
-      return res.send({msg: "No such user exists"});
-  
-    res.send({data: userDetails });
+    if (!userDetails){
+      return res.status(400).send({msg: "No such user exists"});
+    }else{
+    res.status(201).send({data: userDetails });
+    }
+  }
+  catch(error){
+    res.status(500).send({msg:error})
+  }
   }
   
 
 let updatedUser=async function(req,res){
+  try{
   let attribute=req.body
    let updated=await userModel.findOneAndUpdate({_id:req.params.userId},{$set:attribute},{new:true})
-  res.send(updated)
+  res.status(201).send(updated)
+  }
+  catch(error){
+    res.status(500).send({msg:error})
+  }
 }
 
 let deletedUser=async function(req,res){
-  let deleted=await userModel.updateOne({_id:req.userId},{$set:{isDeleted:true}})
-  res.send(deleted)
+  try{
+  let deleted=await userModel.findOneAndUpdate({_id:req.userId},{$set:{isDeleted:true}})
+  res.status(200).send(deleted)
+  }
+  catch(error){
+    res.status(500).send({msg:error})
+  }
 }
   
   module.exports.createUsers=createUsers

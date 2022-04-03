aacd39debada64cf9a968152938f7847de8912a8
12-shrati <shrati02@gmail.com>
2022-04-03 19:13:49 +0530

    assignment 3/April

---
+++ src/routes/route.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const router = express.Router();
+const bookController = require('../controller/bookController')
+const s3=require('../aws/s3')
+
+
+
+router.post('/image/aws', s3.imageUrl)
+
+router.post('/books',  bookController.createBook)
+
+
+
+module.exports = router;
